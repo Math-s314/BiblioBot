2d8abf986b888750fa758eea153423588dd50d00
Math-ss <math_s@outlook.fr>
2021-06-02 23:18:55 +0200

    Incomming message management update -> split steps -> better organisation

--- commands/command.js
+++ commands/command.js
@@ -21,65 +21,88 @@ const DM = require('./action/userspecific');
  * @param {Discord.Message} message 
  */
 function OnMessage(message) {
+    /**
+     * First part : check if the message is for us, and update the log if necessary.
+     */
+
     //To not answer its own messages
     if(message.author.id == Import.client.user.id)
         return;
 
-    //For DM messages
-    if(message.guild == undefined || message.guild == null) {
-        let options = message.content.split('-');
-        for (let i = 0; i < options.length; i++) {
-            options[i] = options[i].replace(/\s/g, '');
-            options[i] = options[i].toLocaleLowerCase();
+    //Select correct messages whenn they come from a guild channel
+    let guild = null;
+    if(message.guild != undefined && message.guild != null){
+        if (Import.GuildParameters.get(message.guild.id) == undefined || !message.content.startsWith(Import.GuildParameters.get(message.guild.id).prefix))
+            return;
+        else {
+            //Update guild's log to signal the message
+            guild = message.guild.id;
+            Import.GuildLogStream.get(guild).write('\n');
+            Import.GuildLogStream.get(guild).write('OnMessage');
+            Import.GuildLogStream.get(guild).write(JSON.stringify(arguments, null, 4)); 
         }
-
-        if(DM.commandList.includes(options[0]))
-            DM.command(message, options);
-        
-        return;
     }
-    
-    //If the message isn't for the bot
-    if (Import.GuildParameters.get(message.guild.id) == undefined || !message.content.startsWith(Import.GuildParameters.get(message.guild.id).prefix))
-        return;
-
-    //For guild messages
-    const guild = message.guild.id;
-    Import.GuildLogStream.get(guild).write('\n');
-    Import.GuildLogStream.get(guild).write('OnMessage');
-    Import.GuildLogStream.get(guild).write(JSON.stringify(arguments, null, 4));
 
     /**
-     * @type {string[]}
-     **/
+     * Second part : message's parsing
+     */
+    
+    //Necessary variables
     let options = [];
     let subject = [0, 0];
     let channel_eval = message.channel.id;
     
-    options = message.content.substring(Import.GuildParameters.get(message.guild.id).prefix.length).split('-');
+    //Message's divion and normalization
+    options = (guild == null) ? message.content.split('-') : message.content.substring(Import.GuildParameters.get(message.guild.id).prefix.length).split('-');
     for (let i = 0; i < options.length; i++) {
         options[i] = options[i].replace(/\s/g, '');
         options[i] = options[i].toLocaleLowerCase();
     }
-    
-    if(Settings.commandList.indexOf(options[0]) > -1)
+
+    /**
+     * Third part : Enable the right command
+     */    
+
+    //DM commands
     {
+        //Check command
+        if(DM.commandList.includes(options[0])) {
+            DM.command(message, options);
+            return;
+        }
+
+        //If it's an DM, just leave
+        if(guild == null) {
+            BasicFunction.SendRightChannel(message, options, 'error', 'Unknown command !', (msg) => {}, [], "", false);
+            return;
+        }
+    }
+    
+    //Settings command
+    if(Settings.commandList.indexOf(options[0]) > -1) {
         Settings.command(message, options);
         return;
     }
     
-    subject = BasicFunction.DoubleListContain(Import.GuildParameters.get(guild).channel_subject, channel_eval);
-    if (subject[0] == -1 && Import.GuildParameters.get(guild).channel_prof.indexOf(channel_eval) == -1)
-        return;
+    //Commands based on channels
+    {
+        //Check channel
+        subject = BasicFunction.DoubleListContain(Import.GuildParameters.get(guild).channel_subject, channel_eval);
+        if (subject[0] == -1 && Import.GuildParameters.get(guild).channel_prof.indexOf(channel_eval) == -1) {
+            return;
+        }
 
-    if(Prof.commandList.indexOf(options[0]) > -1) {
-        Prof.command(message, options);
-        return;
-    }
+        //Prof commands
+        if(Prof.commandList.indexOf(options[0]) > -1) {
+            Prof.command(message, options);
+            return;
+        }
 
-    if(Pupils.commandList.indexOf(options[0]) > -1) {
-        Pupils.command(message, options, subject[0]);
-        return;
+        //Pupils commands
+        if(Pupils.commandList.indexOf(options[0]) > -1) {
+            Pupils.command(message, options, subject[0]);
+            return;
+        }
     }
 
     BasicFunction.SendRightChannel(message, options, 'error', 'Unknown command !');
